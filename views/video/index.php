af56703aebbab6bc8dc179f99b2dab8921f364d6
maliang <maliang@xywy.com>
2016-10-19 11:15:24 +0800

    ml

--- views/video/index.php
+++ views/video/index.php
@@ -6,7 +6,7 @@
     <title>媒体播放器</title>
     <link rel="stylesheet" href="/css/zy.media.min.css">
 </head>
-<body>
+<body style="overflow-y:hidden">
 <div class="zy_media">
     <video poster="/images/test.jpg" data-config='{"mediaTitle": "《疯狂动物城》--腾讯视频"}'>
         <source src="/public/video/test.mp4" type="video/mp4">
@@ -31,7 +31,8 @@
         //var conH = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
         // transform: rotate(90deg); width: 667px; height: 375px;transform-origin:28% 50%;
         //var iosTopHe = 0;//若有其他样式判断，写于此
-
+        console.log(conW);
+        console.log(conH);
         $(className).css({
             "transform":"rotate(90deg) translate("+((conH-conW)/2)+"px,"+((conH-conW)/2)+"px)",
             "width":conH+"px",
